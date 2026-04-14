5b57c6e270676a4373fbd6ec77f3cbfd2229f6c2
vjofrefoster <vicente.jofre07@inacapmail.cl>
2026-04-14 13:21:05 -0400

    Initial commit

---
+++ ejercicio1.py
@@ -0,0 +1,16 @@
+
+num1 =input ("ingrese el primer numero ")
+num2 =input ("ingrese el segundo numero ")
+
+if num1>num2:
+    print("el numero 1 es mayor que numero 2")
+elif num1<num2:
+    print("el numero 2 es mayor que numero 1")
+else:
+    print("los numeros son iguales")
+
+
+
+
+
+
